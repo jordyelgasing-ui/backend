345b65bed96b7e7ace6d2375b8ce97b76f426c49
Piljam <devinfristiansetiawan@gmail.com>
2025-09-14 22:27:21 +0700

    qq

--- app.js
+++ app.js
@@ -1,53 +1,33 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const app = express()
-const bodyParser = require('body-parser')
-const methodOverride = require('method-override')
-const Item = require('./model/item');
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(methodOverride('_method'));
-app.use(express.static('public'));
-app.set('view engine', 'ejs');
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
-// Koneksi MongoDB
-mongoose.connect('mongodb://127.0.0.1:27017/my_new_database', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.log(err));
+const app = express();
 
+// Koneksi MongoD
+mongoose.connect(process.env.MONGODB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-app.get('/',(req,res)=>{
-    res.render('index',{title:'test'});
-})
+// Schema bebas (strict: false)
+const faceSchema = new mongoose.Schema({}, { strict: false });
+const Face = mongoose.model('Face', faceSchema, 'faces');
 
-app.get('/add',(req,res)=>{
-    res.render('add',{title:'test'})
-})
-app.post('/add',async (req,res)=>{
-    const {name,email} =req.body
-    await Item.create({name,email})
-    res.redirect('/')
-})
-app.get('/read',async (req,res)=>{
-    const items = await Item.find();
-    res.render('read',{items})
-})
-app.get('/edit/:id',async(req,res)=>{
-    const items = await Item.findById(req.params.id)
-    res.render('edit',{items})
-})
-app.put('/edit/:id',async(req,res)=>{
-    const { name, email} = req.body;
-    await Item.findByIdAndUpdate(req.params.id, { name, email});
-    res.redirect('/')
-})
+// Middleware biar bisa baca JSON body
+app.use(express.json());
 
-app.delete('/delete/:id',async(req,res)=>{
-    await Item.findByIdAndDelete(req.params.id)
-    res.redirect('/')
-})
-const PORT = 3000
-app.listen(PORT,()=>{
-    console.log(`http://localhost:${PORT}`)
-})
+// Endpoint get all data
+app.get('/faces', async (req, res) => {
+  try {
+    const faces = await Face.find(); // ambil semua data
+    res.json(faces);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server jalan di http://localhost:${PORT}`);
+});
